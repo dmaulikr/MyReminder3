ea8f90114f455acfb0ba709a5e85b733c5d0ce9c
DAKoch <dkoch7591@gmail.com>
2015-05-22 11:01:19 -0700

    New project, for MyReminder3, week #5 assignment

---
+++ MyReminder3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyReminder3
+//
+//  Created by Gru on 05/22/15.
+//  Copyright (c) 2015 GruTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
